898324960ba2bed3478a7f2ca8c931ac5716fc25
HP <HP@DESKTOP-3IGV47K>
2020-02-22 20:32:05 +0100

    first commit

---
+++ ProjetConcours/src/main/java/com/fstg/projetConcours/ProjetConcoursApplication.java
@@ -0,0 +1,13 @@
+package com.fstg.projetConcours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetConcoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetConcoursApplication.class, args);
+	}
+
+}
